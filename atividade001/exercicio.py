eaff3719273e2cf52ebbec899ed20b94c2f569b5
Sebastião Marcos <profsebastiaomarcos@gmail.com>
2021-12-09 09:58:35 -0300

    atividades

---
+++ atividade001/exercicio.py
@@ -0,0 +1,26 @@
+#Curso Técnico de Informática
+#Autor: Sebastião Marcos
+#Data início: 26/10/2021
+#Término: 26/10/2021
+#Estudo de Variáveis: Para a conclusão da atividade basta acrescentar o que foi pedido
+
+#Uso do comando input
+
+#Entrada
+nome = str(input('Qual o seu nome: '))
+dataNascimento = int(input('Em que ano você nasceu: '))
+altura = float(input('Qual a sua altura: '))
+peso = float(input('Quanto você pesa: '))
+
+
+#Saída
+print('-'*40)
+print('ESTUDO DE ENTRADA DE DADOS COM INPUT')
+print('='*40)
+print('Seu nome é..........: ', nome)
+print('Ano de nascimento..........: ', dataNascimento)
+print('Você mede..........: ', altura, 'm')
+print('Seu peso é..........: ', peso, 'Kg')
+print('='*40)
+print('Fim do Programa!')
+print('-'*40)
---
+++ atividade002/exercicio.py
@@ -0,0 +1,39 @@
+#Curso Técnico de Informática
+#Autor: Sebastião Marcos
+#Data início: 26/10/2021
+#Término: 26/10/2021
+#Estudo de Variáveis: Operadores Aritméticos
+
+#Declarações
+a = 10
+b = 2
+#Operações
+#Soma
+soma = a + b
+#Subtração
+subtracao = a - b
+#Produto
+produto = a * b
+#Divisão
+divisao = a / b
+#Potência
+potencia = a ** b
+#Divisão Inteira
+divisaoInteira = a // b
+#Resto da Divisão
+restoDivisao = a % b
+
+#Saída Formatada
+print('-'*40)
+print('ESTUDO DE OPERADORES ARITMÉTICOS')
+print('='*40)
+print(f'O valor de {a} + {b} = {soma}')
+print(f'O valor de {a} - {b} = {subtracao}')
+print(f'O valor de {a} * {b} = {produto}')
+print(f'O valor de {a} / {b} = {divisao}')
+print(f'O valor de {a} ** {b} = {potencia}')
+print(f'O valor de {a} // {b} = {divisaoInteira}')
+print(f'O valor de {a} % {b} = {restoDivisao}')
+print('='*40)
+print('Fim do Programa!')
+print('-'*40)
